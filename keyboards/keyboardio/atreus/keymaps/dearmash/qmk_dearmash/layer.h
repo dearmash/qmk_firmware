537d9f804b95fc48eba4df37431c5a0613a6d25d
Harold De Armas <dearmash@google.com>
2024-03-31 16:09:26 -0700

    second copy of dearmash without baggage I hope

---
+++ keyboards/keyboardio/atreus/keymaps/dearmash/qmk_dearmash/layer.h
@@ -0,0 +1,13 @@
+#pragma once
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+    _BASE,
+    _SYMBOLS,
+    _DIGITS,
+    _MOVEMENT,
+    _WINDOWS,
+    _SHORTCUTS,
+    _EMPTY,
+    _BLOCKED,
+};
+
